6ec2b12df451053b30755f8a7c3a7eb5d0fba254
12liyab <ktliyab7@gmail.com>
2025-01-14 00:21:23 +0000

    Update jquery.easing.1.3.js

--- education/js/jquery.easing.1.3.js
+++ education/js/jquery.easing.1.3.js
@@ -177,7 +177,7 @@ jQuery.extend( jQuery.easing,
  * 
  * Open source under the BSD License. 
  * 
- * Copyright Ã‚Â© 2024 Liyab Pen
+ * Copyright Ã‚Â© 2024 Liyab Pe
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
